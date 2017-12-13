43b1fe29f096f369efbceff300e085e48aac69c0
sunilbittu <pavanksunil@gmail.com>
2017-12-13 23:06:32 +0530

    refractored components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
   ],
   imports: [
     BrowserModule,
